285344d93a64e4e010943c88bf32df53e78d617a
Hector-Ariceaga <crablogni@hotmail.com>
2018-12-25 13:19:39 -0800

    Add helper methods to controllers, complete patient controller

--- app/controllers/api/v1/patients_controller.rb
+++ app/controllers/api/v1/patients_controller.rb
@@ -1,17 +1,40 @@
 class Api::V1::PatientsController < ApplicationController
+  before_action :current_user, only: [:create, :show, :edit]
+  before_action :current_patient, only: [:update, :destroy]
+
   def index
     @patients = Patient.all
     render json: @patients, status: 200
   end
 
   def create
+    @user = current_user
+    @patient = @user.patients.create(patient_params)
+    render json: @patient, status: 200
+  end
+
+  def update
+    @patient.update(patient_params)
+    render json: @patient, status: 200
   end
 
   def destroy
+    if @patient.destroy
+      flash[:message] = "Patient successfully deleted."
+      render json: {patientID: @patient.id}  
+    else
+      flash[:message] = "Patient was deletion was unsuccessful."
+      redirect_to user_path(@patient)
+    end
   end
 
   private
 
   def patient_params
+    params.require(:patient).permit(:patient_id, :name, :dob, :user_id, symptom_ids:[], diagnosis_ids:[])
+  end
+
+  def current_patient
+    @patient = Patient.find_by(id: params[:id])
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  protect_from_forgery with: :null_session
+  helper_method :current_user
+  
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
